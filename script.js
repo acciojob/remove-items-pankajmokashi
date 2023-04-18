a90836b1920ea0fc664319d15c948b8e43e15630
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-18 18:21:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect")
+let button = document.querySelector("input")
+
+function delete_ele(){
+    for(let i = 0; i < select.length; i++)
+    {
+        if(select.children[i].innerText == select.value)
+        {
+            select.children[i].remove()
+        }
+    }
+}
+button.addEventListener("click", delete_ele)
